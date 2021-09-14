5de8475c8c962769e6d3c996ce70a9a9198f08e6
mandanafasounaki <mandana.fasounaki@gmail.com>
2021-09-14 23:35:27 +0300

    Remove extra function

--- load_data.py
+++ load_data.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 import pandas as pd
 
-def load_random():
+def load_random(count=5):
     '''
     Loads a list of random items
     '''
@@ -16,12 +16,12 @@ def load_random():
     
     ##create a random length list from random items    
 #     count = random.choice(range(1,10))
-    count = 5
+    _count = count
     ids = []
     temp2 = []
     rows = []
 #     _dict = {'prodcutid':[], 'name':[], 'subcategory': [], 'category':[]}
-    for i in range(count):
+    for i in range(_count):
         temp = val_data[random.choice(range(len(val_data)))]
         ids.append(temp)
     for _id in ids:
@@ -32,36 +32,4 @@ def load_random():
 
     df_all = pd.DataFrame(rows, columns = ['productid', 'brand', 'category', 'subcategory', 'name'])
     df_all.reset_index()
-    return df_all
-
-
-def load_random_c(count):
-    '''
-    Loads a list of random items
-    '''
-    ##load the validation metadata
-    conn = sqlite3.connect('data.db')
-    c = conn.cursor()
-    c.execute("SELECT * FROM validation")
-    val_data = c.fetchall()
-
-    ##create a random length list from random items
-    #     count = random.choice(range(1,10))
-    _count = count
-    ids = []
-    temp2 = []
-    rows = []
-    #     _dict = {'prodcutid':[], 'name':[], 'subcategory': [], 'category':[]}
-    for i in range(_count):
-        temp = val_data[random.choice(range(len(val_data)))]
-        ids.append(temp)
-    for _id in ids:
-        c.execute("SELECT productid, brand, category, subcategory, name FROM Meta WHERE productid=?", _id)
-        fetched = c.fetchall()
-        for row in fetched:
-            rows.append(row)
-
-    df_all = pd.DataFrame(rows, columns=['productid', 'brand', 'category', 'subcategory', 'name'])
-    df_all.reset_index()
-
     return df_all
