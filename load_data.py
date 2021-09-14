f60fa23de48b8b7c8011b542695ae599018a1bc2
mandanafasounaki <mandana.fasounaki@gmail.com>
2021-09-14 23:24:16 +0300

    Add load_random_c function

--- load_data.py
+++ load_data.py
@@ -5,6 +5,9 @@ import json
 import pandas as pd
 
 def load_random():
+    '''
+    Loads a list of random items
+    '''
     ##load the validation metadata
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
@@ -27,7 +30,38 @@ def load_random():
         for row in fetched:
             rows.append(row)
 
-    
     df_all = pd.DataFrame(rows, columns = ['productid', 'brand', 'category', 'subcategory', 'name'])
     df_all.reset_index()
+    return df_all
+
+
+def load_random_c(count):
+    '''
+    Loads a list of random items
+    '''
+    ##load the validation metadata
+    conn = sqlite3.connect('data.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM validation")
+    val_data = c.fetchall()
+
+    ##create a random length list from random items
+    #     count = random.choice(range(1,10))
+    _count = count
+    ids = []
+    temp2 = []
+    rows = []
+    #     _dict = {'prodcutid':[], 'name':[], 'subcategory': [], 'category':[]}
+    for i in range(_count):
+        temp = val_data[random.choice(range(len(val_data)))]
+        ids.append(temp)
+    for _id in ids:
+        c.execute("SELECT productid, brand, category, subcategory, name FROM Meta WHERE productid=?", _id)
+        fetched = c.fetchall()
+        for row in fetched:
+            rows.append(row)
+
+    df_all = pd.DataFrame(rows, columns=['productid', 'brand', 'category', 'subcategory', 'name'])
+    df_all.reset_index()
+
     return df_all
