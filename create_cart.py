8ebd1f48a602341593007b739b9eb8942c2d612c
mandanafasounaki <mandana.fasounaki@gmail.com>
2021-09-14 23:23:44 +0300

    Update create_cart

--- create_cart.py
+++ create_cart.py
@@ -1,13 +1,12 @@
 from load_data import load_random_c
 import json
 
-def create_random_cart(count, name):
+def create_random_cart(count, c_name):
     
     item_list = load_random_c(count)
     
     dict_items = item_list.to_dict()
     
-    with open(name + '.json', 'w') as f:
-        json.dump(d, f)
-    
-    
+    with open(c_name + '.json', 'w') as f:
+        json.dump(dict_items, f)
+        print("%s.json is created." % (c_name))
